4dbac1092449ab22adda8d62ecf48d157b65ae8a
Fausto1980 <alfa_kyo@yahoo.com>
2024-08-05 22:11:06 -0500

    Add: CLASE_3 Y CLASE_4

---
+++ TEMA_03_TIPO_DE_DATOS/FAUSTO_JARAMILLO/CLASE/4_listas.dart
@@ -0,0 +1,40 @@
+
+void main() {
+
+/// Tipos de listas
+/// Simple:
+/// List<int> --> (String)
+/// Tipo de dato --> lista
+
+// Lista vacia
+
+//constante
+const numerosConstantes = [];
+
+//Inmutable
+final numerosfinal = [1];
+
+
+// para que infiera el tipo de dato no debe estar vacia
+
+// para definir tipo de dato en una lista vacia
+
+final numeros3 = <int> [];
+
+// diferencia entre dejar como dynamic o definir tipo de dato
+final numeros4 = [1, 2, 3, 4, 5];
+final List = [1, 2, 3, 4, 5];
+
+final listaDeStrings = ["Lunes", "Martes", "Miércoles"];
+
+
+List.add(6);
+print(List);
+
+
+
+
+
+
+
+}
