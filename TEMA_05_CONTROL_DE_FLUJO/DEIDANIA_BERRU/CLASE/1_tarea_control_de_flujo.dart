1fed9e4686c0f0b75958f29d2e575544cf594b07
deidaniatest <deidania2@gmail.com>
2024-08-07 20:48:56 -0500

    Update tarea de IF  adicional 07-08-2024

--- TEMA_05_CONTROL_DE_FLUJO/DEIDANIA_BERRU/CLASE/1_tarea_control_de_flujo.dart
+++ TEMA_05_CONTROL_DE_FLUJO/DEIDANIA_BERRU/CLASE/1_tarea_control_de_flujo.dart
@@ -31,4 +31,73 @@
           print("La contraseña ingresada es incorrecta favor validar");
        }
     }
-  } 
+   /*
+   ### Ejercicios if
+1. Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:
+
+| Renta                   |	Tipo impositivo |
+|-------------------------|-----------------|
+| Menos de 10000€         |	5%              |
+| Entre 10000€ y 20000€   | 15%             |
+| Entre 20000€ y 35000€   | 20%             |
+| Entre 35000€ y 60000€   | 30%             |
+| Más de 60000€           | 45%             |
+
+Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla el tipo impositivo que le corresponde.
+*/
+    print("Ingresa su renta anual :");
+    final valor = stdin.readLineSync();
+    if (valor?.length == 0){
+       print("Favor validar, es necesario ingresar un valor");
+    }  
+    double rentaAnual = double.parse(valor!);
+    double impositivo =0;
+    if (rentaAnual   <  10000 ) {
+       impositivo = rentaAnual*0.05;
+    } else if (rentaAnual >= 10000 && rentaAnual <= 20000) {
+         impositivo = rentaAnual*0.15;
+    } else if (rentaAnual >= 20000 && rentaAnual <= 35000) {
+       impositivo = rentaAnual*0.20;
+    } else if (rentaAnual >= 35000 && rentaAnual <= 60000){
+        impositivo = rentaAnual*0.30;
+    } else if (rentaAnual > 60000 ){
+       impositivo = rentaAnual*0.45;
+    }
+    print("El varlo de su renta anual $rentaAnual y el valor de Impositivo es : $impositivo");
+    /*
+    2. En una determinada empresa, sus empleados son evaluados al final de cada año. Los puntos que pueden obtener en la evaluación comienzan en 0.0 y pueden ir aumentando, traduciéndose en mejores beneficios. Los puntos que pueden conseguir los empleados pueden ser 0.0, 0.4, 0.6 o más, pero no valores intermedios entre las cifras mencionadas. A continuación se muestra una tabla con los niveles correspondientes a cada puntuación. La cantidad de dinero conseguida en cada nivel es de 2.400€ multiplicada por la puntuación del nivel.
+
+| Nivel         | Puntuación    |
+|---------------|---------------|
+|Inaceptable    |0.0            |
+|Aceptable      |0.4            |
+|Meritorio	    |0.6 o más      |
+
+Escribir un programa que lea la puntuación del usuario e indique su nivel de rendimiento, así como la cantidad de dinero que recibirá el usuario.
+*/
+  final Map<String,dynamic>evaluacion = {
+ "Juan":{
+  "Puntuacion": 0,
+  "Nivel":"Inaceptable",
+  "Dinero":2400},
+  "Sofia":{
+  "Puntuacion": 0.4,
+  "Nivel":"Aceptable",
+  "Dinero":2400},  
+  "David":{
+  "Puntuacion": 0.6,
+  "Nivel":"Meritorio",
+  "Dinero":2400}
+  };
+  print("Ingresa el Nombre de empleado para validar su evaluación :");
+  final empleado = stdin.readLineSync();
+  if (empleado?.length == 0){
+       print("Favor validar, es necesario ingrese el nombre de usuario");
+  }
+  if (evaluacion[empleado] ==  null){
+     print("Usuario Ingresado No existe, favor reviar");     
+  }else {
+    double remuneracion = evaluacion[empleado]['Puntuacion']*evaluacion[empleado]['Dinero'];
+    print("El usuario : $empleado  tiene una evaluacion de ${evaluacion?[empleado]["Nivel"]} su remuneracion a recibir es  $remuneracion ");
+  }       
+}      
